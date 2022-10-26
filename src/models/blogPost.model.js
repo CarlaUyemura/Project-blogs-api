3826a561bf5930a53a55d946db11eac32d46c557
Carla Uyemura <carla.uyemura@gmail.com>
2022-10-26 16:47:29 -0300

    Requisito 1 3/4

---
+++ src/models/blogPost.model.js
@@ -0,0 +1,23 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+    user_id: {
+     type: DataTypes.INTEGER,
+     foreignKey: true,
+    }
+  }, {
+    tableName: 'blog_posts',
+    timestamps: false,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {foreignKey: 'user_id', as: 'users'})
+  }
+
+  return BlogPost;
+};
+
+module.exports = BlogPostModel;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,8 +4,15 @@ const UserModel = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
   });
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id', as: 'blogPosts'
+    });
 
+  }
   return User;
 };
 
